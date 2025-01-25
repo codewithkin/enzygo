4e4e4a205c15e9674b2a2c22d84bc7a99b193f9f
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-01-25 12:10:27 +0200

    Enhance user model with verification token and session management fields

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,10 +1,35 @@
 import {Schema, model} from "mongoose";
 
+// Code explanation
+/**
+ * Email - The user's email
+ * verificationToken - A token generated when the user attemps to sign up or sign in. This token is sent to their email to validate that they own it. It expires after 2 minutes (120 seconds) by default
+ * session - The user's active session, this is used to ensure data integrity between the frontend and the backend, it expires after 3 days by default
+ * chats - An array of chat rooms the user is in (you can think of these as whatsapp cahats (a group is its own entity, it doesn't count))
+ * created_at - The date and time the user's account was created
+ */
+
 const UserModel = Schema({
     email : {
         type : String,
         required : true,
         unique : true
+    },
+    verificationToken: {
+        type: {
+            token: String,
+            expires: Date,
+        },
+        required: true,
+        unique: true,
+    },
+    session: {
+        type: {
+            token: String,
+            expires: Date,
+        },
+        required: true,
+        unique: true,
     },
     chats: {
         type: Array,
