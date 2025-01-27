c67883e16747690add0b8cffe0b0856aa43d962f
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-01-27 13:24:02 +0200

    Add Button component and update authentication form; integrate custom fonts and styles

--- client/src/components/ui/forms/Input.tsx
+++ client/src/components/ui/forms/Input.tsx
@@ -13,7 +13,7 @@ interface InputProps {
 export default function Input ({className, name, id, type = "text", placeholder, value, onChange, onBlur, disabled}: InputProps) {
     return (
         <input 
-            className={`${className}`} 
+            className={`placeholder:text-gray-500 focus-visible:ring-1 focus-visible:ring-purple-500 outline-0 ring-0 border border-gray-300 rounded-md pl-4 py-2 min-w-[300px] ${className}`} 
             name={name}
             id={id}
             type={type}
--- client/src/routes/auth/index.tsx
+++ client/src/routes/auth/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import FormField from '../../components/ui/forms/FormField'
 import Label from '../../components/ui/forms/Label'
 import Input from '../../components/ui/forms/Input'
+import Button from '../../components/ui/forms/Button'
 
 export const Route = createFileRoute('/auth/')({
   component: RouteComponent,
@@ -9,20 +10,27 @@ export const Route = createFileRoute('/auth/')({
 
 function RouteComponent() {
   return (
-    <section className="flex flex-col md:flex-row gap-4 items-center">
-      <article className="flex flex-col items-between">
+    <section className="md:h-screen md:px-10 py-20 flex flex-col md:flex-row gap-4 items-center">
+      <article className="flex flex-col gap-8 justify-center h-full">
         {/* App icon and name */}
         <article className="flex gap-2 items-center">
-          <h4>Anzygo</h4>
+          <h4 className='font-poppins font-semibold text-purple-600'>Anzygo</h4>
         </article>
 
         {/* Auth headings */}
         <article className="flex flex-col">
-          <h2 className='font-bold text-purple-700 text-2xl font-display'>Sign in</h2>
-          <p className="text-gray-500">Sign in to continue chatting</p>
+          <h2 className='font-bold text-purple-600 text-3xl font-inter'>Sign in</h2>
+          <p className="text-sm text-gray-500 font-roboto font-light">Sign in to continue chatting</p>
         </article>
 
         {/* Auth form */}
+        <form className='flex flex-col gap-2' action="">
+          <FormField>
+            <Label htmlFor="email">Email</Label>
+            <Input name="email" id="email" type="email" placeholder="Enter your email" />
+          </FormField>
+          <Button loadingText="Signing you in..">Sign in</Button>
+        </form>
       </article>
       <article></article>
     </section>
