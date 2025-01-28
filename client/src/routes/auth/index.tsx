263da9d2bd254640ecd1eb0e708ef18cfe49b493
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-01-28 07:40:38 +0200

    Add web manifest and favicon; enhance authentication route with framer-motion animations

--- client/src/routes/auth/index.tsx
+++ client/src/routes/auth/index.tsx
@@ -4,6 +4,7 @@ import Label from '../../components/ui/forms/Label'
 import Input from '../../components/ui/forms/Input'
 import Button from '../../components/ui/forms/Button'
 import MainPoints from '../../components/ui/auth/MainPoints'
+import {motion} from 'framer-motion'
 
 export const Route = createFileRoute('/auth/')({
   component: RouteComponent,
@@ -11,29 +12,72 @@ export const Route = createFileRoute('/auth/')({
 
 function RouteComponent() {
   return (
-    <section className="md:h-screen md:px-10 py-4 flex flex-col md:flex-row gap-4 items-center">
-      <article className="flex flex-col gap-8 justify-center h-full md:w-1/2 md:px-20">
+    <section className="md:h-screen md:px-10 py-4 flex flex-col-reverse md:flex-row gap-4 items-center">
+      <article className="flex flex-col gap-8 justify-center md:h-full md:w-1/2 md:px-20 h-screen">
         {/* App icon and name */}
-        <article className="flex gap-2 items-center">
+        <motion.article
+        initial={{
+          x: -100
+        }}
+
+        animate={{
+          x: 1,
+        }}
+
+        transition={{
+          duration: 1,
+        }}
+
+        className="flex items-center">
+          <img 
+            src="/Icon.png"
+            alt="Anzygo icon"
+            className='w-8 h-8'
+          />
           <h4 className='font-poppins font-semibold text-purple-600'>Anzygo</h4>
-        </article>
+        </motion.article>
 
         {/* Auth headings */}
-        <article className="flex flex-col">
+        <motion.article 
+        initial={{
+          y: 100
+        }}
+
+        animate={{
+          y: 1,
+        }}
+
+        transition={{
+          duration: 1,
+        }}
+        className="flex flex-col">
           <h2 className='font-bold text-purple-600 text-3xl font-inter'>Sign in</h2>
           <p className="text-sm text-gray-500 font-roboto font-light">Sign in to continue chatting</p>
-        </article>
+        </motion.article>
 
         {/* Auth form */}
-        <form className='flex flex-col gap-2' action="">
+        <motion.form
+         initial={{
+          y: 100
+        }}
+
+        animate={{
+          y: 1,
+        }}
+
+        transition={{
+          duration: 1,
+        }}
+        className='flex flex-col gap-2' action="">
           <FormField>
             <Label htmlFor="email">Email</Label>
             <Input name="email" id="email" type="email" placeholder="Enter your email" />
           </FormField>
           <Button loadingText="Signing you in..">Sign in</Button>
-        </form>
+        </motion.form>
       </article>
-      <article className="bg-purple-600 px-20 text-center rounded-xl h-full md:w-1/2 flex flex-col justify-center items-center">
+
+      <article className="bg-purple-600 px-20 text-center rounded-xl h-full md:w-1/2 flex flex-col justify-center items-center py-20">
         <MainPoints />
       </article>
     </section>
